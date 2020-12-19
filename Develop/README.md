b5ab35cf526dfd63d99bc438ac501b4a47eabcdc
prasanna <bagilip@gmail.com>
2020-12-19 13:41:16 -0800

    adding files

--- Develop/README.md
+++ Develop/README.md
@@ -1,11 +1,29 @@
-
-# title ==> ReadMe Generator
-## Description==> AutoGenerates a readMe file by taking user input
-## Usage ==>  This can be used to autogenerate a readme file based on the answers to the questions asked
-## License ==> MIT
-## Contributions ==> Prasanna
-## Tests==> test
-## Questions==> no
-## Github ==> https://img.shields.io/badge/Github-ReadMe Generator-4cbbb9
-## Picture ==> https://github.com/ReadMe Generator.png?size=50
-## email ==> bagilip@gmail.com
+# *REadME Generator*
+  ## Description:
+  Auto generates a readme file
+  
+  ## Table of contents:
+  * Installation
+  * Usage
+  * License
+  * Contributing
+  * Tests
+  * Questions
+  ## Installation:
+  > inquirer
+  ## Usage:
+  Tweak this project to ask a buch of questions to autogenerate a readme
+  ## License:
+  MIT
+  ## Contributions:
+  bunch of online resources
+  ## Tests:
+  nodemon index.js
+  ## Questions:
+  no
+  ## Github:
+  https://img.shields.io/badge/Github-Prasmah-4cbbb9
+  ## Avatar:
+  https://github.com/Prasmah.png?size=50
+  ## Email:
+  bagilip@gmail.com
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,21 +1,43 @@
-function generateMarkdown(data) {
 
+module.exports = generateMarkdown;
+
+function generateMarkdown(data) {
+ 
   const avatar = `https://github.com/${data.username}.png?size=50`;
   const gitHub = `https://img.shields.io/badge/Github-${data.username}-4cbbb9`;
   const license = `http://img.shields.io/badge/license-${data.license}-brightgreen.svg`;
 
-  return `
-# title ==> ${data.title}
-## Description==> ${data.description}
-## Usage ==>  ${data.usage}
-## License ==> ${data.license}
-## Contributions ==> ${data.contributions}
-## Tests==> ${data.tests}
-## Questions==> ${data.questions}
-## Github ==> ${gitHub}
-## Picture ==> ${avatar}
-## email ==> ${data.email}
+  return `# *${data.title}*
+  ## Description:
+  ${data.description}
+  
+  ## Table of contents:
+  * Installation
+  * Usage
+  * License
+  * Contributing
+  * Tests
+  * Questions
+  ## Installation:
+  > ${data.installation}
+  ## Usage:
+  ${data.usage}
+  ## License:
+  ${data.license}
+  ## Contributions:
+  ${data.contributions}
+  ## Tests:
+  ${data.tests}
+  ## Questions:
+  ${data.questions}
+  ## Github:
+  ${gitHub}
+  ## Avatar:
+  ${avatar}
+  ## Email:
+  ${data.email}
 `;
+
+
 }
 
-module.exports = generateMarkdown;
